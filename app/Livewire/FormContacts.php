6c7f873083fa0743c01509799451e13fb2c4ba14
Lekito <alexdossantosdias1212@gmail.com>
2025-03-10 18:49:24 -0300

    validation of the new contact form

--- app/Livewire/FormContacts.php
+++ app/Livewire/FormContacts.php
@@ -4,8 +4,19 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
-class FormContacts extends Component
-{
+class FormContacts extends Component{
+
+    public $name, $email, $phone;
+
+    public function newContact(){
+        //validation form
+        $this->validate([
+            'name'   => 'required|min:3|max:50',
+            'email'  => 'required|email|max:50',
+            'phone'  => 'required|min:5|max:20',
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.form-contacts');
